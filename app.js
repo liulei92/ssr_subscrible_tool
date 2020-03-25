e253cb0d64153ea52cb4b3cfadf0e3622394e195
Lei Liu <leiliu@grandstream.cn>
2020-03-25 09:43:59 +0800

    add ssr.txt, vmess.txt
    Cause: None
    Solution: None
    Fixed Version: None
    Branch: None

--- app.js
+++ app.js
@@ -5,25 +5,27 @@
 /**
  * 开发依赖
  */
-let path = require('path')
-let util = require('./util')
-let base64 = util.base64
-let readSync = util.readSync
-let writeSync = util.writeSync
+const path = require('path')
+const util = require('./util')
+const base64 = util.base64
+const readSync = util.readSync
+const writeSync = util.writeSync
 
-let BUILD_DIR = 'dist' // 构建目录
-let ENTRY_FILE = './node.txt' // SS(R)入口
-let str = readSync(ENTRY_FILE)
+const BUILD_DIR = 'dist' // 构建目录
+const SSR_FILE = './ssr.txt' // ssr 入口
+const VMESS_FILE = './vmess.txt' // 
+const ssr = readSync(SSR_FILE)
+const vmess = readSync(VMESS_FILE)
 
 // 符合 ssr 的 修改group 或者 添加group
-let reducer = (total = '', item, index, arr) => {
+const ssrReducer = (total = '', item, index, arr) => {
   if (item.startsWith('ss')) {
-    let arr = item.split('//')
+    const arr = item.split('//')
     arr[1] = base64.decode(arr[1])
 
     if (arr[1].includes('group=')) {
-      let _arr = arr[1].split('/?')
-      let params = _arr[1].split('&')
+      const _arr = arr[1].split('/?')
+      const params = _arr[1].split('&')
       arr[1] = _arr[0] + '/?' + params.map(el => {
         if (el.includes('group=')) {
           el = 'group=' + base64.encode('pingm组')
@@ -40,9 +42,19 @@ let reducer = (total = '', item, index, arr) => {
   }
 }
 
+// 处理 vmess
+const vmessReducer = (total = '', item, index, arr) => {
+  return total
+}
+
 // Map all the item include `ssr://` and serialize those items
-let result = str.split('\n')
-                .reduce(reducer, '')
+const ssrResult = ssr.split('\n')
+                .reduce(ssrReducer, '')
                 .replace(/\,/g, '\r\n')
 
-writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64.encode(result))
+const vmessResult = vmess.split('\n')
+                .reduce(vmessReducer, '')
+                .replace(/\,/g, '\r\n')
+
+writeSync(path.resolve(__dirname, BUILD_DIR, 'ssr.txt'), base64.encode(ssrResult))
+writeSync(path.resolve(__dirname, BUILD_DIR, 'vmess.txt'), base64.encode(vmessResult))
