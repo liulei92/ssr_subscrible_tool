89d54253cc6f8b993d001cab21a6041810e2fb4b
CheriSh_Meng <814050025@qq.com>
2020-04-17 01:31:56 +0800

    更新目录结构

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 /**
  * 开发依赖
  */
+const fs = require('fs');
 const path = require('path')
 const util = require('./util')
 const base64 = util.base64
@@ -51,6 +52,53 @@ const vmessReducer = (total = '', item, index, arr) => {
   }
 }
 
+const readFiles = (filePath, fileType) => {
+  // 根据文件路径读取文件，返回文件列表
+  fs.readdir(path.resolve(filePath), (err, files) => {
+    if (err) {
+      console.warn(err);
+    } else {
+      // 遍历读取到的文件列表
+      files.forEach(filename => {
+        console.log(filename);
+        //获取当前文件的绝对路径
+        const filedir = path.join(filePath, filename);
+        // 根据文件路径获取文件信息，返回一个fs.Stats对象
+        fs.stat(filedir, (eror, stats) => {
+          if (eror) {
+            console.warn('获取文件stats失败');
+          } else {
+            const isFile = stats.isFile(); // 是文件
+            // const isDir = stats.isDirectory(); // 是文件夹
+            if (isFile) {
+              console.log(filedir);
+              // 读取文件内容
+              let content = readSync(filedir);
+              console.log(content);
+              if (fileType === 'ssr') {
+                content = content.split('\n')
+                .reduce(ssrReducer, '')
+                .replace(/\,/g, '\r\n')
+              } else if (fileType === 'v2mess') {
+                content = content.split('\n')
+                .reduce(vmessReducer, '')
+                .replace(/\,/g, '\r\n')
+              }
+              writeSync(path.resolve(__dirname, BUILD_DIR + '/' + fileType, filename), base64.encode(content))
+            }
+            // if (isDir) {
+            //   fileDisplay(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+            // }
+          }
+        })
+      });
+    }
+  })
+}
+
+readFiles('./ssr', 'ssr')
+readFiles('./vmess', 'vmess')
+
 // Map all the item include `ssr://` and serialize those items
 const ssrResult = ssr.split('\n')
                 .reduce(ssrReducer, '')
@@ -59,8 +107,6 @@ const ssrResult = ssr.split('\n')
 const vmessResult = vmess.split('\n')
                 .reduce(vmessReducer, '')
                 .replace(/\,/g, '\r\n')
-
-console.log(base64.encode(vmessResult))
 
 writeSync(path.resolve(__dirname, BUILD_DIR, 'ssr.txt'), base64.encode(ssrResult))
 writeSync(path.resolve(__dirname, BUILD_DIR, 'vmess.txt'), base64.encode(vmessResult))
